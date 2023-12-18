d2322cb2d9f7789af2a86240593fa00783215394
CodeByGit <CodeBy@example.com>
2023-12-18 13:46:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.travelapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, HomeActivity::class.java)
+        val skip = findViewById<TextView>(R.id.textViewSkip)
+        skip.setOnClickListener(View.OnClickListener { startActivity(intent) })
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
